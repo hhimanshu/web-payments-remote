1b3b5f2378a33bec061f67791dfdc36093e5ff73
hhimanshu <dev.atwork@yahoo.com>
2023-04-28 14:04:50 -0700

    feat: update config

--- capacitor.config.ts
+++ capacitor.config.ts
@@ -6,18 +6,13 @@ const config: CapacitorConfig = {
     webDir: 'build',
     bundledWebRuntime: false,
     server: {
-        "url": process.env.NODE_ENV === 'development' ? 'http://localhost:3000/' : "https://web-payments-khaki.vercel.app/"
+        "url": process.env.NODE_ENV === 'development' ? 'http://localhost:3000/' : "https://web-payments-remote-five.vercel.app/"
     },
     plugins: {
         CordovaPurchase: {
             version: '13.4.0',
-            // configuration for each platform goes here
         },
-        // other plugins go here
     },
 };
 
 export default config;
-
-// todo
-// add in-ap purchase on Google Play Store
