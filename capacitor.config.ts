9ececc9e7c92e84e51c79cacada5adb84b9987b6
hhimanshu <dev.atwork@yahoo.com>
2023-04-28 23:36:51 -0700

    feat: changing server.url for testing with apple

--- capacitor.config.ts
+++ capacitor.config.ts
@@ -6,7 +6,8 @@ const config: CapacitorConfig = {
     webDir: 'build',
     bundledWebRuntime: false,
     server: {
-        "url": process.env.NODE_ENV === 'development' ? 'http://localhost:3000/' : "https://web-payments-remote-hhimanshu.vercel.app/"
+        //"url": process.env.NODE_ENV === 'development' ? 'http://localhost:3000/' : "https://web-payments-remote-hhimanshu.vercel.app/"
+        "url": process.env.NODE_ENV === 'development' ? 'http://localhost:3000/' : "https://web-payments-remote-git-h2-apple-hhimanshu.vercel.app/"
     },
     plugins: {
         CordovaPurchase: {
